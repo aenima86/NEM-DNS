d6a70e20dede8cf2abd8106c78a24d6bbf7c7b32
aenima86 <appsileci@gmail.com>
2018-03-22 18:05:42 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+var nem = require("nem-sdk").default;
+
+
+// from url
 var urls = getParameterByName('url');
 
 
@@ -7,42 +11,52 @@ if(urls !== null){
  	var namespace = urls.replace(/.nem/g, '');
 	var namRequest = "http://104.128.226.60:7890/namespace?namespace=" + namespace;
 	
-	getNamespaceOwner(namRequest,namespace,1);
+	var pointerAcc = getPointerAccount(namespace);
 
-}
+	console.log( "namRequest:"+namRequest );
+
+	getNamespaceOwner(namRequest,namespace,1,pointerAcc);
+
+} 
 
 
 
-
+// from button request 
+// redirect to ip
 $("#goBut").click(function(){
 	
 	var namespace = $('#inputName').val().replace(/.nem/g, '');
-	
 
 	var namRequest = "http://104.128.226.60:7890/namespace?namespace=" + namespace;
 
+	var pointerAcc = getPointerAccount(namespace);
+
 	console.log( "namRequest:"+namRequest );
 
-	getNamespaceOwner(namRequest,namespace,1);
-
+	getNamespaceOwner(namRequest,namespace,1,pointerAcc);
 
 
 });
 
+// from button request 
+// get info
 $("#getBut").click(function(){
 	
 	var namespace = $('#inputName').val().replace(/.nem/g, '');
-
+	
 	var namRequest = "http://104.128.226.60:7890/namespace?namespace=" + namespace;
-
+	
+	var pointerAcc = getPointerAccount(namespace);
+	
 	console.log( "namRequest:"+namRequest );
-
-	getNamespaceOwner(namRequest,namespace,2);
+	
+	getNamespaceOwner(namRequest,namespace,2,pointerAcc);
 	
 });
 
 
-function getNamespaceOwner(namRequest,namespace,typeRe) {
+
+function getNamespaceOwner(namRequest,namespace,typeRe,pointerAcc) {
 
 
 	var jqxhr = $.getJSON( namRequest, function() {
@@ -52,8 +66,9 @@ function getNamespaceOwner(namRequest,namespace,typeRe) {
 
 		  ownerAdd = data['owner'];
 		  console.log( "Owner:"+ownerAdd );
+			console.log( "PointerAcc:"+pointerAcc );
 
-		  getDNSdata(ownerAdd,namespace,typeRe) ;
+		 getDNSdata(ownerAdd,namespace,typeRe,pointerAcc) ;
 
 
 		})
@@ -74,9 +89,9 @@ function getNamespaceOwner(namRequest,namespace,typeRe) {
 };
 
 
-function getDNSdata(ownerAdd,namespace,typeRe) {
+function getDNSdata(ownerAdd,namespace,typeRe,pointerAcc) {
 
-	var dataRequest = "http://104.128.226.60:7890/account/transfers/outgoing?address="+ownerAdd;
+	var dataRequest = "http://104.128.226.60:7890/account/transfers/incoming?address="+pointerAcc;
 	
 	
 		var jqxhr = $.getJSON( dataRequest, function() {
@@ -84,35 +99,27 @@ function getDNSdata(ownerAdd,namespace,typeRe) {
 		  })
 			.done(function(data) {
 	
-			  
+			  var stop =0;
 
 			  $.each(data['data'], function( index, value ) {
-				
 
-					if (value['transaction']['mosaics']!= undefined){
+					
+					if ( nem.model.address.toAddress(value['transaction']['signer'], nem.model.network.data.testnet.id)== ownerAdd){
+						
 
-						$.each(value['transaction']['mosaics'], function( index2, value2 ) {
+						var payload = convertFromHex(value['transaction']['message']['payload']);
+						var objPayload = jQuery.parseJSON( payload );
+						if (objPayload['dns']=='yes' && stop==0){
 
-							
-							if (value2['mosaicId']['namespaceId'] == namespace && value2['mosaicId']['name'] == 'dns'){
+							stop =1;
 
-								var payload = convertFromHex(value['transaction']['message']['payload']);
-								console.log(payload);
-								var objPayload = jQuery.parseJSON( payload );
-								console.log(objPayload['ip1']);
+							if(typeRe==1){window.location ='http://'+objPayload['ip1']};
+							if(typeRe==2){alert(payload);};
 
-								if(typeRe==1){window.location ='http://'+objPayload['ip1']};
-								if(typeRe==2){alert(payload);};
-								
-								
-								
-
-
-							}
-
-						});
+						}
 
 					}
+				
 					
 
 			  });
@@ -150,4 +157,35 @@ function getParameterByName(name, url) {
 	if (!results) return null;
 	if (!results[2]) return '';
 	return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function getPointerAccount(namespace){
+
+	passphrase = sha256(namespace);
+	
+	var privateKey =  nem.crypto.helpers.derivePassSha(passphrase, 6000).priv;
+	
+	var keyPair = nem.crypto.keyPair.create(privateKey);
+	
+	var publicKey = keyPair.publicKey.toString();
+	
+	var address = nem.model.address.toAddress(publicKey, nem.model.network.data.testnet.id);
+	
+	return address;
+
+}
+
+
+// finding namespace pointer address (PA)
+$("#goPA").click(function(){
+	
+	findPA();
+
+});
+
+function findPA() {
+	var namespace = prompt("Please enter your namespace (e.g. 'helloworld'", "MyNameSpace");
+	if (namespace != null) {
+			alert("The pointer address for " +namespace +" is " +getPointerAccount(namespace));
+	}
 }
